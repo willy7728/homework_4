67c35da143ac0a50ddb3a8a91819e2a44acf7c9d
willy7728 <43198451+willy7728@users.noreply.github.com>
2018-10-25 00:32:51 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+//         偶數相加
         Scanner scn = new Scanner(System.in);
         int a = scn.nextInt();
         int b = scn.nextInt();
