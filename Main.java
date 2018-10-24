ec3c1d1f2fb3260743847ac4840bcf6a786914f3
willy7728 <43198451+willy7728@users.noreply.github.com>
2018-10-25 00:27:42 +0800

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        int v1 = 0;
+        for (int i = a; i <= b; i = i + 1) {
+            if (i%2==0 && a<b) {
+                v1 = v1 + i;
+            }
+        }
+        System.out.println(v1);
+    }
+}
